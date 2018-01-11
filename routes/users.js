b5556c55426e39b90223858a41e411f3ba59777c
Sachin Choudhary <sachin26049@gmail.com>
2018-01-11 13:32:18 +0530

    mean2

---
+++ routes/users.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+// Register
+router.post('/register', (req, res, next) => {
+ res.send('Register');
+});
+
+// Authenticate
+router.post('/authenticate', (req, res, next) => {
+  res.send('AUTHENTICATE');
+});
+
+// Profile
+router.get('/profile', (req, res, next) => {
+  res.send('PROFILE');
+});
+
+
+
+module.exports = router;
