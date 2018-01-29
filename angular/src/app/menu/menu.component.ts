c968b76bf610ba726cf185985ed852a85f838c7f
Sachin Choudhary <sachin26049@gmail.com>
2018-01-29 11:58:50 +0530

    menu4

--- angular/src/app/menu/menu.component.ts
+++ angular/src/app/menu/menu.component.ts
@@ -9,6 +9,7 @@ import {MenusService} from '../services/menus.service';
 export class MenuComponent implements OnInit {
 type:[Object];
 Menu:[Object];
+Count:[Number];
 constructor(private MS: MenusService,) {
 
  }
@@ -17,27 +18,45 @@ constructor(private MS: MenusService,) {
     this.MS.getMenu().subscribe(data => {
     if(data['success'])
     {
-      console.log("hhh");
+      //console.log("hhh");
     this.Menu=data['menu'];
     //console.log(data['menu']);
     //console.log(this.Menu);
     }
 
+this.Count=[new Number(this.Menu.length)];
 
+for(let i=0;i<this.Menu.length;i++)
+{
+this.Count[i]=0;
+}
   });
 
   this.MS.getType().subscribe(data => {
     if(data['success'])
     {
-      console.log("hhh");
+      //console.log("hhh");
     this.type=data['type'];
     //console.log(data['menu']);
-    console.log(this.type);
+    //console.log(this.type);
     }
 
 
   });
+
   }
+  incrCount(index:number){
+    this.Count[index]=(Number)(this.Count[index])+1;
+    console.log(index + ":" + this.Count[index]);
+  }
+  decrCount(index:number)
+  {
+    if(this.Count[index]!=0)
+    {
+      this.Count[index]=(Number)(this.Count[index])-1;
+      console.log(index + ":" + this.Count[index]);
+    }
+}
   /*LoadMenu(){
 
     this.MS.getMenu().subscribe(data => {
