46831a9736def47966abc36a130f231c31681c5e
Sachin Choudhary <sachin26049@gmail.com>
2018-02-18 16:37:08 +0530

    chef working2

--- angular/src/app/chef/chef.component.ts
+++ angular/src/app/chef/chef.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ChefsService} from './chefs.service';
 import {ChefSocketService} from './chef-socket.service';
+import { Body } from '@angular/http/src/body';
 @Component({
   selector: 'app-chef',
   templateUrl: './chef.component.html',
@@ -9,11 +10,13 @@ import {ChefSocketService} from './chef-socket.service';
 export class ChefComponent implements OnInit {
 orderList:any[];
 orderStatus:any[];
+flag:any[];
   constructor(private CS:ChefsService,private CSO:ChefSocketService) {}
 
   ngOnInit() {
     this.orderList=new Array();
     this.orderStatus=new Array();
+    this.flag=new Array();
     this.CS.init();
     //this.orderList.push("hehe");
     /*this.CS.newOrder();
@@ -23,6 +26,7 @@ orderStatus:any[];
   .subscribe((order: any) => {
     console.log(order);
     this.orderList.push(order);
+    this.flag.push(0);
     this.orderStatus[this.orderStatus.length]=new Array();
   });
 
@@ -44,6 +48,12 @@ orderStatus:any[];
       "orders":orders
     }
     this.CSO.sendOrderStatus(order);
+    this.flag[i]=1;
+  }
+
+  OrderDeliverd(i:any)
+  {
+    this.flag[i]=2;
   }
   
 
